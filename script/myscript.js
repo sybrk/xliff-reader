3dc08c7aa498d213c9c7b06ffd27660aae24c407
Abrek Koç <44588876+abrek-koc@users.noreply.github.com>
2023-10-27 12:28:53 +0200

    text edit and download functionality
    Added a new script that enables users to edit the source and target text and download the final version of this file.

--- script/myscript.js
+++ script/myscript.js
@@ -1,24 +1,27 @@
 // adding functions to some buttons after the document is fully loaded.
 document.addEventListener('DOMContentLoaded', function() {
 
-    // adding click event for read button
-    const read_button = document.querySelector("#readbutton");
-    read_button.addEventListener('click', () => create_table())
-// adding click event for reset button
-    const reset_button = document.querySelector("#resetbutton");
-    reset_button.addEventListener('click', () => reset())
-	
+	// adding click event for read button
+	const read_button = document.querySelector("#readbutton");
+	read_button.addEventListener('click', () => create_table())
+	// adding click event for reset button
+	const reset_button = document.querySelector("#resetbutton");
+	reset_button.addEventListener('click', () => reset())
+	// adding click event for download button
+	const download_button = document.querySelector("#downloadbutton");
+	download_button.addEventListener('click', () => download_file())
+
 })
 
 async function read_file(file) {
-	
+
 	// create a file reader Object
 	let reader = new FileReader();
 	// read file as text
 	reader.readAsText(file)
 	// await the file to be read.
 	await new Promise(resolve => reader.onload = () => resolve());
-	
+
 	// return filename and filecontent with keys.
 	return await {
 		filename: file.name,
@@ -67,7 +70,7 @@ async function create_table() {
 					const row = document.createElement("tr");
 					// give a class to each row.
 					row.classList.add("myrows");
-					
+
 					// create new table cell for file name info
 					const filename_data = document.createElement("td");
 					// create new table cell for target segment status.
@@ -87,7 +90,7 @@ async function create_table() {
 					tablerow.appendChild(status_data);
 					tablerow.appendChild(sourcetext_data);
 					tablerow.appendChild(targettext_data);
-					
+
 					// fill in filename column using filename key from read_file function.
 					filename_data.innerHTML = reading.filename;
 
@@ -97,11 +100,75 @@ async function create_table() {
 					} else {
 						status_data.innerHTML = "undefined";
 					}
-					
+
 					// write source text to Source column
 					sourcetext_data.innerHTML = seg.innerHTML;
 					// write target text to Target column
 					targettext_data.innerHTML = target_seg[i].innerHTML;
+
+					// add a click event listener to the source text cell
+					sourcetext_data.addEventListener('click', (event) => {
+						// get the current text content of the cell
+						const currentText = event.target.textContent.trim();
+
+						// create an input element with the current text as its value
+						const input = document.createElement('input');
+						input.type = 'text';
+						input.value = currentText;
+
+						// replace the cell's text content with the input element
+						event.target.textContent = '';
+						event.target.appendChild(input);
+
+						// focus the input element and select its contents
+						input.focus();
+						input.select();
+
+						// add a blur event listener to the input element
+						input.addEventListener('blur', () => {
+							// get the new text content of the input element
+							const newText = input.value.trim();
+
+							// replace the input element with the new text content
+							event.target.removeChild(input);
+							event.target.textContent = newText;
+
+							// enable the download button
+							document.querySelector("#downloadbutton").disabled = false;
+						});
+					});
+
+					// add a click event listener to the target text cell
+					targettext_data.addEventListener('click', (event) => {
+						// get the current text content of the cell
+						const currentText = event.target.textContent.trim();
+
+						// create an input element with the current text as its value
+						const input = document.createElement('input');
+						input.type = 'text';
+						input.value = currentText;
+
+						// replace the cell's text content with the input element
+						event.target.textContent = '';
+						event.target.appendChild(input);
+
+						// focus the input element and select its contents
+						input.focus();
+						input.select();
+
+						// add a blur event listener to the input element
+						input.addEventListener('blur', () => {
+							// get the new text content of the input element
+							const newText = input.value.trim();
+
+							// replace the input element with the new text content
+							event.target.removeChild(input);
+							event.target.textContent = newText;
+
+							// enable the download button
+							document.querySelector("#downloadbutton").disabled = false;
+						});
+					});
 				});
 			});
 		}
@@ -110,11 +177,44 @@ async function create_table() {
 
 // function to remove all table rows from the HTML page.
 function reset () {
-	
+
 	let rows = document.querySelector("#tbody").querySelectorAll("tr");
-	
+
 	rows.forEach(function(element) {
 		element.remove();
 	});
+
+	// disable the download button
+	document.querySelector("#downloadbutton").disabled = true;
 }
-	
+
+// function to download the modified xliff content as a file
+function download_file() {
+	// get the modified xliff content from the table
+	const tableRows = document.querySelectorAll("#tbody tr");
+	let modifiedContent = "";
+	tableRows.forEach(function(row) {
+		const sourceText = row.querySelector(".sourcetext").textContent.trim();
+		const targetText = row.querySelector(".targettext").textContent.trim();
+		modifiedContent += `<trans-unit><source>${sourceText}</source><target>${targetText}</target></trans-unit>`;
+	});
+
+	// create a Blob object from the modified xliff content
+	const blob = new Blob([`<?xml version="1.0" encoding="UTF-8"?>\n<xliff version="1.2">\n${modifiedContent}\n</xliff>`], {type: "text/xml"});
+
+	// create a URL object from the Blob object
+	const url = URL.createObjectURL(blob);
+
+	// create a link element with the URL object as its href attribute
+	const link = document.createElement("a");
+	link.href = url;
+	link.download = "modified.xliff";
+
+	// simulate a click on the link element to download the file
+	document.body.appendChild(link);
+	link.click();
+	document.body.removeChild(link);
+
+	// disable the download button
+	document.querySelector("#downloadbutton").disabled = true;
+}
